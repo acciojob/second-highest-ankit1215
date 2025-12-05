a60aa5be4776e87a5cc22a9f5dd4c64084c9a508
Ankit Pandit <58819767+ankit1215@users.noreply.github.com>
2025-12-05 12:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let highest = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > highest) {
+      second = highest;
+      highest = num;
+    } else if (num < highest && num > second) {
+      second = num;
+    }
+  }
+
+  // If no second highest (all elements same), second stays -Infinity
+  return second === -Infinity ? -Infinity : second;
+}
